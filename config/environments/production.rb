891f19390497f57c06b5720671d22bc3ba287c8b
Nick Kassatly <nick@harleybarton.com>
2014-02-14 16:22:46 -0500

    Heroku Ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for Heroku
+  #Remember to set to your actual host.
+  config.action_mailer.default_url_options = { :host => 'radiant-harbor-1876.herokuapp.com' }
 end
